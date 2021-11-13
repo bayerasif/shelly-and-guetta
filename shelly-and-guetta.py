feaa33abe2a75ec0afc34631d49dd2cc92c408d0
bayerasif <asif.bayer@mail.huji.ac.il>
2021-11-13 18:54:33 +0200

    some file restyling

--- shelly-and-guetta.py
+++ shelly-and-guetta.py
@@ -1,15 +1,27 @@
+import os
+import time
+from datetime import datetime
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime, timedelta
-import time
 from telegram import Bot, ParseMode
-import os
 
 
 INTERVAL = 180
 token = os.environ['TELEGRAM_TOKEN']
 bot = Bot(token=token)
+
+
+def main():
+    previous_df = get_programs_df()
+    while True:
+        df = get_programs_df()
+        if todays_episode_is_up(df, previous_df):
+            msg = generate_message(df)
+            bot.send_message(chat_id="@shellyandguetta", text=msg, parse_mode=ParseMode.HTML)
+        previous_df = df
+        time.sleep(INTERVAL)
+
 
 def get_programs_df():
     r = requests.get('https://www.kan.org.il/Radio/getMoreItems.aspx?index=0&progId=1479')
@@ -22,11 +34,13 @@ def get_programs_df():
     df['date'] = pd.to_datetime(df['date'], dayfirst=True)
     return df
 
+
 def todays_episode_is_up(df, previous_df):
     today = datetime.today().date()
     is_up = df['date'].isin([today]).any()
     there_before = previous_df['date'].isin([today]).any()
     return is_up and not there_before
+
 
 def generate_message(df):
     today = datetime.today().date()
@@ -35,15 +49,6 @@ def generate_message(df):
     msg = f'הפרק בשם "{title}" של תאריך {date} עלה וזמין להאזנה'
     return msg
 
-def main():
-    previous_df = get_programs_df()
-    while True:
-        df = get_programs_df()
-        if todays_episode_is_up(df, previous_df):
-            msg = generate_message(df)
-            bot.send_message(chat_id="@shellyandguetta", text=msg, parse_mode=ParseMode.HTML)
-        previous_df = df
-        time.sleep(INTERVAL)
 
 if __name__ == '__main__':
     main()
